85c15e61937ed8225d9ccac206f7f099072f1791
GUESDON_Th <GUESDON_Th@lprs.local>
2020-09-29 11:32:59 +0200

    hyg

--- Projet.c
+++ Projet.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+int main (void){
+	int nb1;
+	int nb2;
+	int total;
+	char operateur;
+	
+	printf("saisir nb1");
+	scanf("%d", &nb1);
+	
+	getchar();
+	printf("saisir operateur");
+	scanf("%c", &operateur);
+	
+	printf("saisir nb2");
+	scanf("%d", &nb2);
+	
+	if (operateur == '+')
+	{
+		total = nb1 + nb2;
+		printf("%d", total);
+	}
+
+	if (operateur == '/')
+	{
+		total = nb1 / nb2;
+		printf("%d", total);
+	}
+		
+	if (operateur == '*')
+	{
+		total = nb1 * nb2;
+		printf("%d", total);
+	}
+	
+	if (operateur == '-')
+	{
+		total = nb1 - nb2;
+		printf("%d", total);
+	}
+	
+	
+	
+	
+}
